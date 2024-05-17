7e65fb13e9d5b5d7e7ee02701b6242e8068e0f2d
Tamás Barta <tamas@bartatamas.hu>
2024-05-17 10:06:29 +0200

    log cards

--- Player.js
+++ Player.js
@@ -4,12 +4,30 @@ class Player {
   }
 
   static betRequest(gameState, bet) {
+    const cards = getCardsInGame(gameState);
+    console.log('CARDS', cards);
+
+    if (cards.length >= 6) {
+      bet(Math.max(gameState.current_buy_in, gameState.small_blind * 2));
+    }
+
+
     bet(Math.max(gameState.current_buy_in, gameState.small_blind * 2));
+
+
   }
 
   static showdown(gameState) {
-    console.log('showdown', gameState);
+    console.log('SHOWDOWN', gameState);
   }
+}
+
+function findOurselves(gameState) {
+  return gameState.players.find(player => player.hole_cards);
+}
+
+function getCardsInGame(gameState) {
+  return findOurselves(gameState).hole_cards + gameState.community_cards;
 }
 
 module.exports = Player;
